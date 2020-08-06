469941b3bb396adc647869a7d2b40127325be556
soroosh-gb <soroosh.gb@gmail.com>
2020-08-06 17:35:33 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+
+  attr_accessor :title, :author
+
+    def title=(title)
+    @title = title
+    end
+
+    def title
+      @title
+    end
+
+    def author=(name)
+      @name = name
+      end
+  
+      def author
+        @name
+      end
+
+
+  
+end
